d65d400b68ad036ab93f1c72b4c911d4bdb8f92e
MAIRA EMILLY COSTA E SILVA <14993700679>
2022-11-01 09:20:14 -0300

    Adding route predict

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template #importações
+from flask import Flask, jsonify, render_template, request #importações
 import os
 
 app = Flask(__name__, template_folder="templates") # criando o app
@@ -7,6 +7,13 @@ app = Flask(__name__, template_folder="templates") # criando o app
 @app.route('/') # rotas
 def index():
     return render_template("index.html")
+
+@app.rout('/predict', methods=['POST', 'GET'])
+def predict():
+    if request.method == 'POST':
+        return jsonify(request.form)
+    elif request.method == 'GET':
+        return jsonify({"mensagem" : "Utilize o formulário"})
 
 
 if __name__ == '__main__':
